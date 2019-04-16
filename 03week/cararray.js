9054296e3cbfeb12a3a15f7775c2f6a2163b9de1
Emccart10 <esmccarthy@gmail.com>
2019-04-15 20:54:36 -0500

    first changes

--- 03week/cararray.js
+++ 03week/cararray.js
@@ -1,2 +1,11 @@
-let cars = ["Ford", "Porsche", "Toyota", "Smart"];
-console.log(cars.length);
+//let cars = ["Ford", "Porsche", "Toyota", "Smart"];console.log(cars.length);
+
+//let moreCars = ["GM", "Sazuki", "BMW", "Honda"];console.log(cars.length);
+
+//let totalCars = ["moreCars", "cars"]
+//console.log (totalCars.concat)//
+
+var cars = ["Ford", "Porsche", "Toyota", "Smart"]
+var moreCars = ["GM", "Sazuki", "BMW", "Honda"]
+console.log(cars.concat(moreCars));
+
