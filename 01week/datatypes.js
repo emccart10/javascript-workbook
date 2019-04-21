d0aba4b8777ad3b9be85a811449d08f2ed37d3d4
Emccart10 <esmccarthy@gmail.com>
2019-04-20 19:25:46 -0500

    updated

---
+++ 01week/datatypes.js
@@ -0,0 +1,45 @@
+//1// var today = new Date();
+//console.log= (new Date)//
+
+//2// //var boo = 3 + "3";
+//console.log= (3 +"3")//
+
+//4// 
+
+//3// //Number('1234');// 
+
+//5// var i=2;  
+//var j=3;  
+//var k = Number(i) + Number(j); //
+
+//6// Write a JavaScript program that runs only when 2 things are true.
+//var myVariable = 5; 
+//var yourVariable = 100; 
+
+//if(myVariable && yourVariable) { 
+//    console.log("Great") 
+ // };//
+
+//let isNiceWeather = true;
+//let mood = happy;
+//isNiceWeather ? (
+//  mood = happy,
+//  alert('Move to Florida')
+//) :
+
+
+//7//Write a JavaScript program that runs when 1 of 2 things are true.
+//var i = 1; 
+//if (i < 20) 
+ // document.write("20 beats 1"); 
+//else
+ // document.write("i is bigger");// 
+
+
+
+
+//8//Write a JavaScript program that runs when both things are not true.//
+
+
+
+ 
